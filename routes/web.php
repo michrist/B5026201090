82f22897398792ff8ccbbf3ac4c9ad2090027fc1
michrist <christopher.michael104@gmail.com>
2021-11-15 15:27:08 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('praktikum1', function () {
+    return view('prak1');
+});
+Route::get('tugaspweb4', function () {
+    return view('tugas4');
+});
+Route::get('praktikum2', 'ViewController@showPraktikum2');
+Route::get('ETS', 'ViewController@showETS');
+Route::get('tugasphp', 'ViewController@showTugasPHP');
+Route::post('factorialphp', 'ViewController@factorial');
+
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
